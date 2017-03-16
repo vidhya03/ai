55ec940621594c005d54f527020a47cf362b32ef
Deivamani <VDE@EUR.AD.SAG>
2017-03-16 20:03:21 +0530

    first class

--- assignment/google-python-exercises/runtime-exercises/firstclass.py
+++ assignment/google-python-exercises/runtime-exercises/firstclass.py
@@ -2,14 +2,29 @@
 class firstclass:
     """ Initializing of method """
     def __init__(self):
-        self._bar = 1
+        #self._bar = 1
+        self._frontier = {'Up', 'Down'}
+
     @property
     def bar(self):
+        """ Bar property setter """
         return self._bar
     @bar.setter
     def bar(self, value):
+        print('setting value of ',value)
         self._bar = value
 
+    @bar.getter
+    def bar(self):
+        print("in getter of bar")
+        return self._bar
+
+
+
+foo = firstclass()
+print(foo._frontier)
+foo.bar = 10
+print(foo.bar)
 
 
 
