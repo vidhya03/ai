f83d771988d9df9b9430b90766a050c9caf65e3d
vidhyadharan <it.vidhyadharan@gmail.com>
2017-04-03 00:44:32 +0530

    changed the logic

--- python/machine_learning/problem1.py
+++ python/machine_learning/problem1.py
@@ -1,73 +1,67 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as PYPlot
 import sys
+import numpy as NUM_PY
 
-def f(x):
+
+
+def PlottingThePoints(Xaxis, Yaxis, weights):
+
+    for index in xrange(Xaxis.shape[0]):
+        if Yaxis[index] == 1:
+            PYPlot.plot(Xaxis[index, 0], Xaxis[index, 1], 'ro')
+        else:
+            PYPlot.plot(Xaxis[index, 0], Xaxis[index, 1], 'bx')
+    
+    x_axis_min = Xaxis.min(axis=0)
+    x_axis_max = Xaxis.max(axis=0)
+    
+    x_x = [x_axis_min[0] - 1, x_axis_max[0] + 1]
+    y_y = -(weights[2] + weights[0] * x_x) / weights[1]
+    PYPlot.plot(x_x, y_y, 'k')
+    PYPlot.axis([x_axis_min[0] - 1, x_axis_max[0] + 1, x_axis_min[1] - 1, x_axis_max[1] + 1])
+    PYPlot.show()
+
+
+def checkAndReturn(x):
     return 1 if x > 0 else -1
 
-def draw(X, y, w):
-    """
-    This function creates a plot in which the current decision boundary is shown.
-    It allows to see which points are misclassified and how the process is done.
-    """
 
-    for i in xrange(X.shape[0]):
-        if y[i] == 1:
-            plt.plot(X[i, 0], X[i, 1], 'ro')
-        else:
-            plt.plot(X[i, 0], X[i, 1], 'bx')
+def PPerceptronLearningAlgo(X_Sample, Y_label, output_file_result, graphics=False):
+    data = X_Sample.shape[1]
+    weights = NUM_PY.zeros((data + 1, 1))
+    convergence = False
+
+    open(output_file_result, 'w').close()
+    while not convergence:
+        convergence = True
+        for index in xrange(X_Sample.shape[0]):
+            if Y_label[index]*checkAndReturn(weights[-1] + NUM_PY.dot(X_Sample[[index], :], weights[:-1, [0]])) <= 0:
+                weights[-1] = weights[-1] + Y_label[index]
+                weights[:-1, [0]] = weights[:-1, [0]] + Y_label[index] * NUM_PY.transpose(X_Sample[[index], :])
+                convergence = False
+
+        if graphics:
+            PlottingThePoints(X_Sample, Y_label, weights)
     
-    xmin = X.min(axis=0)
-    xmax = X.max(axis=0)
-    
-    xx = [xmin[0] - 1, xmax[0] + 1]
-    yy = -(w[2] + w[0]*xx)/w[1]
-    plt.plot(xx, yy, 'k')
-    plt.axis([xmin[0] - 1, xmax[0] + 1, xmin[1] - 1, xmax[1] + 1])    
-    plt.show()
-
-def PLA(X, y, output_file, animate=False):
-    """
-    Implements Perceptron Learning Algorithm
-    Returns a list of weights, that represent the hyperplane that separates the data
-    linearly
-    """
-    d = X.shape[1]
-    w = np.zeros((d + 1, 1))
-    conv = False
-
-    open(output_file, 'w').close()
-    while not conv:
-        conv = True
-        for i in xrange(X.shape[0]):
-            if y[i]*f(w[-1] + np.dot(X[[i], :], w[:-1, [0]])) <= 0:
-                w[-1] = w[-1] + y[i]
-                w[:-1, [0]] = w[:-1, [0]] + y[i]*np.transpose(X[[i], :])
-                conv = False
-
-        if animate:
-            draw(X, y, w)
-    
-        with open(output_file, "a") as text_file:
-            for wi in w:
-                if wi[0].is_integer():
-                    text_file.write(str(int(wi[0])) + ",")
+        with open(output_file_result, "a") as output_text_file:
+            for weightIdx in weights:
+                if weightIdx[0].is_integer():
+                    output_text_file.write(str(int(weightIdx[0])) + ",")
                 else:
-                    text_file.write(str(wi[0]) + ",")
-            text_file.write("\n")
+                    output_text_file.write(str(weightIdx[0]) + ",")
+            output_text_file.write("\n")
 
-    return w
+    return weights
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print 'Usage: python <input.csv> <output.csv>'
+        print 'Usage: python <input.csv> <output.csv> or python input1.csv output1.csv'
         sys.exit(0)
 
-    dataset = np.genfromtxt(sys.argv[1], delimiter=',', skip_header=0, names=None)
+    sample_dataset = NUM_PY.genfromtxt(sys.argv[1], delimiter=',', skip_header=0, names=None)
 
-    X = dataset[:, [0, 1]]
-    y = dataset[:, [2]]
+    X_data = sample_dataset[:, [0, 1]]
+    y_label = sample_dataset[:, [2]]
 
-    # True for debugging and visualizing the decision boundary
-    w = PLA(X, y, sys.argv[2], False)
-    
+    # for debug set as True else False
+    PPerceptronLearningAlgo(X_data, y_label, sys.argv[2], False)
