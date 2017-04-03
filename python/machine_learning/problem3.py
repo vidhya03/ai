93239a03ff4e918e02b1ea328474dfd6592b5c62
vidhyadharan <it.vidhyadharan@gmail.com>
2017-04-03 14:49:12 +0530

    added project3 output3.csv

--- python/machine_learning/problem3.py
+++ python/machine_learning/problem3.py
@@ -1,162 +1,117 @@
-import matplotlib.pyplot as plt
 import numpy as NUM_PY
-import sys
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn import svm
-from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 
 
-def load_data(input_file):
-    """
-    Loads the dataset. It assumes a *.csv file without header, and the output variable
-    in the last column 
-    """
-    dataset = NUM_PY.genfromtxt(input_file, delimiter=',', skip_header=1, names=None)
-
-    X = dataset[:, :-1]
-    y = dataset[:, -1]
-    return (X, y)
-
-X, y = load_data('input3.csv')
-
-# Stratified split test-train data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
-estimator_svm = svm.SVC()
-
-##############################
-# SVM with linear kernel     #
-##############################
-
-# Searching for the best parameters in the model
-parameters = {'kernel': ('linear',), 'C': [0.1, 0.5, 1, 5, 10, 50, 100]}
-clf = GridSearchCV(estimator_svm, parameters, cv=5)
-clf.fit(X_train, y_train)
-
-print clf.best_estimator_
-print clf.best_score_
-
-# The chosen model
-svc = clf.best_estimator_
-
-# The score obtained using the test set
-print svc.score(X_test, y_test)
-
-##############################
-# SVM with polynomial kernel #
-##############################
-
-# Searching for the best parameters in the model (Takes a while, uncomment for running this)
-parameters = {'kernel': ('poly',), 'C': [0.1, 1, 3], 'degree': [4, 5, 6], 'gamma': [0.1, 1]}
-
-clf = GridSearchCV(estimator_svm, parameters, cv=5)
-clf.fit(X_train, y_train)
-
-print clf.best_estimator_
-print clf.best_score_
-
-# The chosen model
-svc_poly = clf.best_estimator_
-
-# The score obtained using the test set
-print svc_poly.score(X_test, y_test)
-
-##############################
-# SVM with RBF kernel        #
-##############################
-
-# Searching for the best parameters in the model
-parameters = {'kernel': ('rbf',), 'C': [0.1, 0.5, 1, 5, 10, 50, 100], 'gamma': [0.1, 0.5, 1, 3, 6, 10]}
-
-clf = GridSearchCV(estimator_svm, parameters, cv=5)
-clf.fit(X_train, y_train)
-
-print clf.best_estimator_
-print clf.best_score_
-
-# The chosen model
-svc_rbf = clf.best_estimator_
-
-# The score obtained using the test set
-print svc_rbf.score(X_test, y_test)
 
 
-##############################
-# Logistic Regression        #
-##############################
 
-# Searching for the best parameters in the model
-parameters = {'C': [0.1, 0.5, 1, 5, 10, 50, 100]}
-estimator_lr = LogisticRegression()
-clf = GridSearchCV(estimator_lr, parameters, cv=5)
-clf.fit(X_train, y_train)
+def load_data_samples(input_file_example):
+    datasetSamples = NUM_PY.genfromtxt(input_file_example, delimiter=',', skip_header=1, names=None)
 
-print clf.best_estimator_
-print clf.best_score_
+    X_Data = datasetSamples[:, :-1]
+    Y_Data = datasetSamples[:, -1]
+    return (X_Data, Y_Data)
 
-# The chosen model
-svc_lr = clf.best_estimator_
+X_DataSample, Y_data_Label = load_data_samples('input3.csv')
 
-# The score obtained using the test set
-print svc_lr.score(X_test, y_test)
+X_training_data, X_testing_data, y_training_data, y_testing_data = train_test_split(X_DataSample, Y_data_Label, test_size=0.4, random_state=42)
+estimator_svm_values = svm.SVC()
 
-##############################
-# k-Nearest Neighbors        #
-##############################
+parameters_forProcessing = {'kernel': ('linear',), 'C': [0.1, 0.5, 1, 5, 10, 50, 100]}
+gridSearch_CLF = GridSearchCV(estimator_svm_values, parameters_forProcessing, cv=5)
+gridSearch_CLF.fit(X_training_data, y_training_data)
 
-# Searching for the best parameters in the model
-parameters = {'n_neighbors': xrange(1, 51), 'leaf_size': xrange(5, 61, 5)}
-estimator_knn = KNeighborsClassifier()
-clf = GridSearchCV(estimator_knn, parameters, cv=5)
-clf.fit(X_train, y_train)
+print gridSearch_CLF.best_estimator_
+print gridSearch_CLF.best_score_
 
-print clf.best_estimator_
-print clf.best_score_
+gridSearch_BsetEstimator_svc = gridSearch_CLF.best_estimator_
 
-## The chosen model
-svc_knn = clf.best_estimator_
+print gridSearch_BsetEstimator_svc.score(X_testing_data, y_testing_data)
 
-## The score obtained using the test set
-print svc_knn.score(X_test, y_test)
+# SVM with Polynomial Kernel.
+parameters_forProcessing = {'kernel': ('poly',), 'C': [0.1, 1, 3], 'degree': [4, 5, 6], 'gamma': [0.1, 1]}
 
-##############################
-# Decision Trees             #
-##############################
+gridSearch_CLF = GridSearchCV(estimator_svm_values, parameters_forProcessing, cv=5)
+gridSearch_CLF.fit(X_training_data, y_training_data)
 
-# Searching for the best parameters in the model
-parameters = {'max_depth': xrange(1, 51), 'min_samples_split': xrange(2, 11)}
-estimator_dt = DecisionTreeClassifier()
-clf = GridSearchCV(estimator_dt, parameters, cv=5)
-clf.fit(X_train, y_train)
+print gridSearch_CLF.best_estimator_
+print gridSearch_CLF.best_score_
 
-print clf.best_estimator_
-print clf.best_score_
+svc_poly = gridSearch_CLF.best_estimator_
 
-# The chosen model
-dt = clf.best_estimator_
+print svc_poly.score(X_testing_data, y_testing_data)
 
-# The score obtained using the test set
-print dt.score(X_test, y_test)
+#SVM with RBF Kernel.
+parameters_forProcessing = {'kernel': ('rbf',), 'C': [0.1, 0.5, 1, 5, 10, 50, 100], 'gamma': [0.1, 0.5, 1, 3, 6, 10]}
+
+gridSearch_CLF = GridSearchCV(estimator_svm_values, parameters_forProcessing, cv=5)
+gridSearch_CLF.fit(X_training_data, y_training_data)
+
+print gridSearch_CLF.best_estimator_
+print gridSearch_CLF.best_score_
+
+gridSearch_svc_rbf = gridSearch_CLF.best_estimator_
+
+print gridSearch_svc_rbf.score(X_testing_data, y_testing_data)
+
+#Logistic Regression.
+parameters_forProcessing = {'C': [0.1, 0.5, 1, 5, 10, 50, 100]}
+estimator_logisticRegression = LogisticRegression()
+gridSearch_CLF = GridSearchCV(estimator_logisticRegression, parameters_forProcessing, cv=5)
+gridSearch_CLF.fit(X_training_data, y_training_data)
+
+print gridSearch_CLF.best_estimator_
+print gridSearch_CLF.best_score_
+
+svc_lr = gridSearch_CLF.best_estimator_
+
+print svc_lr.score(X_testing_data, y_testing_data)
+
+#k-Nearest Neighbors. Try values of n_neighbors = [1, 2, 3, ..., 50] and leaf_size = [5, 10, 15, ..., 60].
+parameters_forProcessing = {'n_neighbors': xrange(1, 51), 'leaf_size': xrange(5, 61, 5)}
+kNeighbors_estimator = KNeighborsClassifier()
+gridSearch_CLF = GridSearchCV(kNeighbors_estimator, parameters_forProcessing, cv=5)
+gridSearch_CLF.fit(X_training_data, y_training_data)
+
+print gridSearch_CLF.best_estimator_
+print gridSearch_CLF.best_score_
+
+gridSearch_svc_knn = gridSearch_CLF.best_estimator_
+
+print gridSearch_svc_knn.score(X_testing_data, y_testing_data)
 
 
-##############################
-# Random Forests             #
-##############################
 
-# Searching for the best parameters in the model
-parameters = {'max_depth': xrange(1, 51), 'min_samples_split': xrange(2, 11)}
-estimator_rf = RandomForestClassifier()
-clf = GridSearchCV(estimator_rf, parameters, cv=5)
-clf.fit(X_train, y_train)
+#Decision Trees. Try values of max_depth = [1, 2, 3, ..., 50] and min_samples_split = [2, 3, 4, ..., 10].
+parameters_forProcessing = {'max_depth': xrange(1, 51), 'min_samples_split': xrange(2, 11)}
+decisionTreeClassifierEstimator_dt = DecisionTreeClassifier()
+gridSearch_CLF = GridSearchCV(decisionTreeClassifierEstimator_dt, parameters_forProcessing, cv=5)
+gridSearch_CLF.fit(X_training_data, y_training_data)
 
-print clf.best_estimator_
-print clf.best_score_
+print gridSearch_CLF.best_estimator_
+print gridSearch_CLF.best_score_
 
-# The chosen model
-rf = clf.best_estimator_
+decisionTreeEstimator = gridSearch_CLF.best_estimator_
 
-## The score obtained using the test set
-print rf.score(X_test, y_test)
+print decisionTreeEstimator.score(X_testing_data, y_testing_data)
+
+
+
+#Random Forest.  Try values of max_depth = [1, 2, 3, ..., 50] and min_samples_split = [2, 3, 4, ..., 10].
+parameters_forProcessing = {'max_depth': xrange(1, 51), 'min_samples_split': xrange(2, 11)}
+randomForestClassifierEstimator = RandomForestClassifier()
+gridSearch_CLF = GridSearchCV(randomForestClassifierEstimator, parameters_forProcessing, cv=5)
+gridSearch_CLF.fit(X_training_data, y_training_data)
+
+print gridSearch_CLF.best_estimator_
+print gridSearch_CLF.best_score_
+
+rainForestEstimator = gridSearch_CLF.best_estimator_
+
+print rainForestEstimator.score(X_testing_data, y_testing_data)
 
