1f60c4e10626bb56bff9b746a8780685c8810c24
vidhyadharan <it.vidhyadharan@gmail.com>
2017-04-08 21:24:40 +0530

    removed empty files

--- python/sudoku/driver.py
+++ python/sudoku/driver.py
@@ -1,20 +1,13 @@
 import sys
 from SudokuBoard import *
-
-
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print 'Usage: python driver.py <input_string> or python driver.py 003020600900305001001806400008102900700000008006708200002609500800203009005010300'
         sys.exit(0)
-
     sudokuBoard = SudokuBoard(sys.argv[1])
-
-
     cspAlgoritm = SudokuBoardCSPAlgo(sudokuBoard)
     AC3_Algo(cspAlgoritm)
-
     temp = BackCheckS(cspAlgoritm)
-
     for per in temp:
         cspAlgoritm.customDomain[per] = [temp[per]]
 
